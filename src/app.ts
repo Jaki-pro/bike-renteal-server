42e4d6918969cf6a47a60352c76e5b5b306313cd
Jakaria Hossain <mdjak8980@gmail.com>
2024-06-13 03:47:22 +0600

    project setup

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 //application routes
-// app.use('/api/v1/', router);
+// app.use('/api/', router);
 app.get('/', async (req: Request, res: Response) => {
   const a = 10;
   res.json({ a: a });
