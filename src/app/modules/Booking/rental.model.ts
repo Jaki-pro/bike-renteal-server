1b4543ae5ea092fb536669d82e09120772db2e89
Jakaria Hossain <mdjak8980@gmail.com>
2024-06-16 00:27:02 +0600

    complete server

--- src/app/modules/Booking/rental.model.ts
+++ src/app/modules/Booking/rental.model.ts
@@ -2,8 +2,8 @@ import { Schema, model } from 'mongoose';
 import { TRental } from './rental.interface';
 
 const rentalSchema = new Schema<TRental>({
-  userId: { type: Schema.Types.ObjectId, required: true },
-  bikeId: { type: Schema.Types.ObjectId, required: true },
+  userId: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+  bikeId: { type: Schema.Types.ObjectId, required: true, ref: 'Bike' },
   startTime: { type: Date, required: true, default: new Date() },
   returnTime: { type: Date, default: null },
   totalCost: { type: Number, required: true, default: 0 },
